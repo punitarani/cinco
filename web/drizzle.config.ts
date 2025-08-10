f1a1bcf991fe430f652a968fc14bda1e6d3eb0d1
jaidev-jayakumar <jaidevjk@gmail.com>
2025-08-09 18:44:40 -0700

    using db.query and removed duplicate schema

--- web/drizzle.config.ts
+++ web/drizzle.config.ts
@@ -2,7 +2,8 @@ import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
   dialect: "postgresql",
-  schema: "./db/schema.ts",
+  // Use the shared schema in the root app
+  schema: "../src/db/schema",
   out: "./drizzle",
   dbCredentials: {
     url: process.env.DATABASE_URL ?? "postgres://postgres:postgres@localhost:5432/cinco",
